4052fdfab0b6577561d7c0e33fcedbc918d3a013
Anyik Duku <anyik.duku@patternmatched.com>
2022-02-16 09:01:02 +0200

    cucumber_reports/20220216

--- src/test/java/myTestRunners/MyTestRunner.java
+++ src/test/java/myTestRunners/MyTestRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-//@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		features = {"src/test/resources/features/SearchPosts.feature"},
 		glue = {"stepDefinitions"},
